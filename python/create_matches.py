624b31346fa3f3a080738f470236dd02decc979f
krswanson <kristin@swanson.name>
2021-12-16 17:04:46 -0800

    Use previous year's data and pull emails from separate file

--- python/create_matches.py
+++ python/create_matches.py
@@ -19,29 +19,36 @@ def new_index_order(l):
 				new_order = index_shuffle(l)
 	return new_order
 
-def create_matches(filename):
+def get_json_file(filename):
 	dirname = os.path.dirname(__file__)
 	file = os.path.join(dirname, filename)
 	f = open(file, "r")
-	people_json = f.read()
+	json_str = f.read()
 	f.close()
-	people_rela = json.loads(people_json)["people"]
+	json_dict = json.loads(json_str)
+	return json_dict
+
+def create_matches(matchfile):
+	rela_dict = get_json_file("relationships.json")
+	original_names_list = list(rela_dict.keys())
+	old_matches = get_json_file(matchfile)
+	for name in original_names_list:
+		rela_dict[name].append(old_matches[name])
 
 	# Reshuffle until no invalid matches are created
 	while True:
-		new_order = new_index_order(people_rela)
+		new_order = new_index_order(original_names_list)
 		matches = {}
 		done = True
-		for i, rela in enumerate(people_rela):
-			person = list(rela.keys())[0]
-			match = list(people_rela[new_order[i]].keys())[0]
-			email = rela["email"]
-			if match in rela[person]:
+		for i, name in enumerate(original_names_list):
+			match = original_names_list[new_order[i]]
+			if match in rela_dict[name]:
+				#print(name, match, rela_dict[name])
 				done = False
 				break
-			matches[person] = {"match": match, "email": email}
+			matches[name] = match
 		if done:
 			break
 	return matches
-#matches = create_matches('relationships.json')
+#matches = create_matches('matches_2021.json')
 #print(matches)
